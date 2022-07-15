8e9d42535a751f9224e4bddc0f575847dd264efe
IV Pravdin <72220773+ivpravdin@users.noreply.github.com>
2022-07-15 14:12:37 +0200

    Update utils.py

--- nerf/utils.py
+++ nerf/utils.py
@@ -28,7 +28,7 @@ import torch.distributed as dist
 from torch.utils.data import Dataset, DataLoader
 
 import trimesh
-import mcubes
+import marching_cubes as mcubes
 from rich.console import Console
 from torch_ema import ExponentialMovingAverage
 
@@ -172,30 +172,38 @@ def extract_fields(bound_min, bound_max, resolution, query_func, S=128):
     Z = torch.linspace(bound_min[2], bound_max[2], resolution).split(S)
 
     u = np.zeros([resolution, resolution, resolution], dtype=np.float32)
+    rgbs = np.zeros([resolution, resolution, resolution, 3], dtype=np.float32)
     with torch.no_grad():
         for xi, xs in enumerate(X):
             for yi, ys in enumerate(Y):
                 for zi, zs in enumerate(Z):
                     xx, yy, zz = custom_meshgrid(xs, ys, zs)
                     pts = torch.cat([xx.reshape(-1, 1), yy.reshape(-1, 1), zz.reshape(-1, 1)], dim=-1) # [S, 3]
-                    val = query_func(pts).reshape(len(xs), len(ys), len(zs)).detach().cpu().numpy() # [S, 1] --> [x, y, z]
+                    val, color = query_func(pts) # [S, 1] --> [x, y, z]
+                    val = val.reshape(len(xs), len(ys), len(zs)).detach().cpu().numpy()
+                    color = color.reshape(len(xs), len(ys), len(zs), 3).detach().cpu().numpy()
                     u[xi * S: xi * S + len(xs), yi * S: yi * S + len(ys), zi * S: zi * S + len(zs)] = val
-    return u
+                    rgbs[xi * S: xi * S + len(xs), yi * S: yi * S + len(ys), zi * S: zi * S + len(zs)] = color
+    return u, rgbs
 
 
 def extract_geometry(bound_min, bound_max, resolution, threshold, query_func):
     #print('threshold: {}'.format(threshold))
-    u = extract_fields(bound_min, bound_max, resolution, query_func)
+    u, color = extract_fields(bound_min, bound_max, resolution, query_func)
 
-    #print(u.shape, u.max(), u.min(), np.percentile(u, 50))
+    print(u.shape, u.max(), u.min())
+    print(color.shape, color.max(), color.min())
     
-    vertices, triangles = mcubes.marching_cubes(u, threshold)
+    smoothed_surface = mcubes.smooth(u)
+    
+    vertices_color, triangles_color = mcubes.marching_cubes_color(u, color, threshold)
 
     b_max_np = bound_max.detach().cpu().numpy()
     b_min_np = bound_min.detach().cpu().numpy()
+    print(b_max_np.shape, b_min_np.shape)
 
-    vertices = vertices / (resolution - 1.0) * (b_max_np - b_min_np)[None, :] + b_min_np[None, :]
-    return vertices, triangles
+    vertices_color[:,:3] = vertices_color[:,:3] / (resolution - 1.0) * (b_max_np - b_min_np)[None, :] + b_min_np[None, :]
+    return vertices_color, triangles_color
 
 
 class Trainer(object):
@@ -562,7 +570,7 @@ class Trainer(object):
     def save_mesh(self, save_path=None, resolution=256, threshold=10):
 
         if save_path is None:
-            save_path = os.path.join(self.workspace, 'meshes', f'{self.name}_{self.epoch}.ply')
+            save_path = os.path.join(self.workspace, 'meshes', f'{self.name}_{self.epoch}.obj')
 
         self.log(f"==> Saving mesh to {save_path}")
 
@@ -571,13 +579,17 @@ class Trainer(object):
         def query_func(pts):
             with torch.no_grad():
                 with torch.cuda.amp.autocast(enabled=self.fp16):
-                    sigma = self.model.density(pts.to(self.device))['sigma']
-            return sigma
+                    density_map = self.model.density(pts.to(self.device))
+                    sigma = density_map['sigma']
+                    color = density_map['color']
+            return sigma, color
 
         vertices, triangles = extract_geometry(self.model.aabb_infer[:3], self.model.aabb_infer[3:], resolution=resolution, threshold=threshold, query_func=query_func)
+        
+        mcubes.export_obj(vertices, triangles, save_path)
 
-        mesh = trimesh.Trimesh(vertices, triangles, process=False) # important, process=True leads to seg fault...
-        mesh.export(save_path)
+        #mesh = trimesh.Trimesh(vertices, triangles, process=False) # important, process=True leads to seg fault...
+        #mesh.export(save_path)
 
         self.log(f"==> Finished saving mesh.")
 
